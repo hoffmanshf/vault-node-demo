f493e4be77c057d892223b575d5f8412edc72383
hoffman <hoffmanshf@outlook.com>
2020-09-23 10:39:28 -0400

    fix

--- vault-read.sh
+++ vault-read.sh
@@ -8,6 +8,6 @@ platform=$(uname | tr '[:upper:]' '[:lower:]')
 wget https://github.com/kir4h/rvault/releases/latest/download/rvault-${version}-${platform}-amd64.tar.gz
 tar xzf rvault-${version}-${platform}-amd64.tar.gz
 
-./rvault read secret -f json -k 1 -p api-server -a "ciam.getpitstop.io" | tail -n +2 | jq . > $BASE_DIR/data/secret.json
+./rvault read secret -f json -k 1 -p api-server --config config.toml | tail -n +2 | jq . > $BASE_DIR/data/secret.json
 rm rvault-${version}-${platform}-amd64.tar.gz
 rm rvault
