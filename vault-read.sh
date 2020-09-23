d0a1eae447957556015eae13d6fd7a58722345dc
hoffman <hoffmanshf@outlook.com>
2020-09-23 10:58:25 -0400

    fix

--- vault-read.sh
+++ vault-read.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 BASE_DIR=`pwd`
-export VAULT_TOKEN=$VAULT_TOKEN
 
 version=$(curl -s https://api.github.com/repos/kir4h/rvault/releases/latest | jq -r .tag_name)
 platform=$(uname | tr '[:upper:]' '[:lower:]')
 wget https://github.com/kir4h/rvault/releases/latest/download/rvault-${version}-${platform}-amd64.tar.gz
 tar xzf rvault-${version}-${platform}-amd64.tar.gz
 
-./rvault read secret -f json -k 1 -p api-server --config config.toml | tail -n +2 | jq . > $BASE_DIR/data/secret.json
+./rvault read secret -f json -k 1 -p api-server | tail -n +2 | jq . > $BASE_DIR/data/secret.json
 rm rvault-${version}-${platform}-amd64.tar.gz
 rm rvault
