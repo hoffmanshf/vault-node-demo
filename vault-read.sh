10f7df4eff01e6424d83c01dea726850313e35ca
hoffman <hoffmanshf@outlook.com>
2020-09-23 10:28:31 -0400

    fix

--- vault-read.sh
+++ vault-read.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 BASE_DIR=`pwd`
+export VAULT_ADDR=$VAULT_ADDR
+export VAULT_TOKEN=$VAULT_TOKEN
 
 version=$(curl -s https://api.github.com/repos/kir4h/rvault/releases/latest | jq -r .tag_name)
 platform=$(uname | tr '[:upper:]' '[:lower:]')
