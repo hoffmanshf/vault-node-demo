ea26f4cc320817ac49655ec1115c067cec27f0d8
hoffman <hoffmanshf@outlook.com>
2020-09-23 10:32:36 -0400

    fix

--- vault-read.sh
+++ vault-read.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 BASE_DIR=`pwd`
-export VAULT_ADDR='ciam.getpitstop.io'
 export VAULT_TOKEN=$VAULT_TOKEN
 
 version=$(curl -s https://api.github.com/repos/kir4h/rvault/releases/latest | jq -r .tag_name)
@@ -9,6 +8,6 @@ platform=$(uname | tr '[:upper:]' '[:lower:]')
 wget https://github.com/kir4h/rvault/releases/latest/download/rvault-${version}-${platform}-amd64.tar.gz
 tar xzf rvault-${version}-${platform}-amd64.tar.gz
 
-./rvault read secret -f json -k 1 -p api-server | tail -n +2 | jq . > $BASE_DIR/data/secret.json
+./rvault read secret -f json -k 1 -p api-server -a "ciam.getpitstop.io" | tail -n +2 | jq . > $BASE_DIR/data/secret.json
 rm rvault-${version}-${platform}-amd64.tar.gz
 rm rvault
